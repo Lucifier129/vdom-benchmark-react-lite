4fb10637fd93267b4cbbbb16a618a9cfd0b5b593
Boris Kaul <localvoid@gmail.com>
2016-01-27 22:22:01 +0600

    Update to 0.14.6

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,49 @@
-'use strict';
-
 var gulp = require('gulp');
-var gulp_if = require('gulp-if');
-var del = require('del');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var sourcemaps = require('gulp-sourcemaps');
-var uglify = require('gulp-uglify');
-var browserify = require('browserify');
-var deploy = require('gulp-gh-pages');
-
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-
-var NODE_ENV = process.env.NODE_ENV || 'development';
-var BROWSERSYNC_PORT = parseInt(process.env.BROWSERSYNC_PORT) || 3000;
-var RELEASE = (NODE_ENV === 'production');
-var DEST = './build';
-
-gulp.task('clean', del.bind(null, [DEST]));
-
-gulp.task('scripts', function() {
-  var bundler = browserify({
-    entries: ['./web/js/main.js'],
-    debug: true
-  });
-
-  return bundler
-    .bundle()
-    .pipe(source('main.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(gulp_if(RELEASE, uglify()))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(DEST + '/js/'))
-    .pipe(reload({stream: true}));
-});
+var gutil = require('gulp-util');
+var webpack = require('webpack');
+var ghPages = require('gulp-gh-pages');
+var path = require('path');
 
 gulp.task('html', function() {
-  gulp.src('./web/*.html')
-    .pipe(gulp.dest(DEST))
-    .pipe(reload({stream: true}));
+  gulp.src('web/index.html')
+      .pipe(gulp.dest('build'));
 });
 
-gulp.task('serve', ['default'], function() {
-  browserSync({
-    open: false,
-    port: BROWSERSYNC_PORT,
-    notify: false,
-    server: 'build'
-  });
+gulp.task('build', function(callback) {
+  var cfg = {
+    entry: ['./web/js/main.jsx'],
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'bundle.js'
+    },
+    resolve: {
+      extensions: ['', '.js', '.jsx']
+    },
+    module: {
+      loaders: [{
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loaders: ['babel?{ "presets": ["es2015-loose", "react"] }']
+      }]
+    },
+    plugins: [
+      new webpack.NoErrorsPlugin(),
+      new webpack.DefinePlugin({'process.env': {NODE_ENV: JSON.stringify('production')}}),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin()
+    ]
+  };
 
-  gulp.watch('./web/js/**/*.js', ['scripts']);
-  gulp.watch('./web/**/*.html', ['html']);
+  webpack(cfg, function(err, stats) {
+    if (err) throw new gutil.PluginError('build', err);
+    gutil.log('[build]', stats.toString({colors: true}));
+    callback();
+  });
 });
 
 gulp.task('deploy', ['default'], function () {
-  return gulp.src(DEST + '/**/*')
-    .pipe(deploy());
+  return gulp.src('./build/**/*')
+      .pipe(ghPages());
 });
 
-gulp.task('default', ['scripts', 'html']);
+gulp.task('default', ['html', 'build']);
--- web/js/main.jsx
+++ web/js/main.jsx
@@ -1,24 +1,19 @@
-'use strict';
-
 var benchmark = require('vdom-benchmark-base');
-var React = require('react/lib/React');
-var createElement = React.createElement;
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 var NAME = 'React';
-var VERSION = '0.13.1';
+var VERSION = '0.14.6';
 
 function renderTree(nodes) {
   var children = [];
-  var i;
-  var e;
-  var n;
 
-  for (i = 0; i < nodes.length; i++) {
-    n = nodes[i];
+  for (var i = 0; i < nodes.length; i++) {
+    var n = nodes[i];
     if (n.children !== null) {
-      children.push(createElement('div', {key: n.key}, renderTree(n.children)));
+      children.push((<div key={n.key}>{renderTree(n.children)}</div>));
     } else {
-      children.push(createElement('span', {key: n.key}, n.key.toString()));
+      children.push((<span key={n.key}>{n.key}</span>));
     }
   }
 
@@ -39,11 +34,11 @@ BenchmarkImpl.prototype.tearDown = function() {
 };
 
 BenchmarkImpl.prototype.render = function() {
-  React.render(createElement('div', null, renderTree(this.a)), this.container);
+  ReactDOM.render(<div>{renderTree(this.a)}</div>, this.container);
 };
 
 BenchmarkImpl.prototype.update = function() {
-  React.render(createElement('div', null, renderTree(this.b)), this.container);
+  ReactDOM.render(<div>{renderTree(this.b)}</div>, this.container);
 };
 
 document.addEventListener('DOMContentLoaded', function(e) {
